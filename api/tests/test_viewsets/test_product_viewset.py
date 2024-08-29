5edcd5992189354f1086661721db43e4e6963e7b
JulioVieiraP <134452045+JulioVieiraP@users.noreply.github.com>
2024-08-29 18:20:27 -0300

    refactor: mudando a forma do codigo para o pytest, para manter um padrão no projeto

--- api/tests/test_viewsets/test_product_viewset.py
+++ api/tests/test_viewsets/test_product_viewset.py
@@ -1,50 +1,44 @@
 import json
 import pytest
-
 from django.urls import reverse
-
 from rest_framework import status
-from rest_framework.test import APIClient, APITestCase
 
 from api.factories import ProductFactory
 from api.models import Product
 
 @pytest.mark.django_db
-class TestProductViewSet(APITestCase):
-    client = APIClient()
+def test_get_all_products(client):
+    url = reverse('product-list')
     
-    def setUp(self):
-        self.url = "product-list"
-        
-        self.product = ProductFactory(
-            name='Mouse',
-            description='Mouse description',
-            price=100
-        )
+    product = ProductFactory(
+        name='Mouse',
+        description='Mouse description',
+        price=100
+    )
     
-    def test_get_all_products(self):
-        response = self.client.get(reverse(self.url))
-        
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        product_data = json.loads(response.content)
-        
-        self.assertEqual(product_data[0]['name'], self.product.name)
-        self.assertEqual(product_data[0]['description'], self.product.description)
+    response = client.get(url)
     
-    def test_create_product(self):
-        data = json.dumps({
-            'name': 'Teclado',
-            'description': 'Teclado descritivo',
-            'price': 200
-        })
-        
-        response = self.client.post(reverse(self.url), data=data, content_type='application/json')
-        
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        
-        created_product = Product.objects.get(name='Teclado')
-        
-        self.assertEqual(created_product.name, 'Teclado')
-        self.assertEqual(created_product.price, 200)
-        
+    assert response.status_code == status.HTTP_200_OK
+    product_data = response.json()
     
+    assert product_data[0]['name'] == product.name
+    assert product_data[0]['description'] == product.description
+
+@pytest.mark.django_db
+def test_create_product(client):
+    url = reverse('product-list')
+    
+    data = {
+        'name': 'Teclado',
+        'description': 'Teclado descritivo',
+        'price': 200
+    }
+    
+    response = client.post(url, data=json.dumps(data), content_type='application/json')
+    
+    assert response.status_code == status.HTTP_201_CREATED
+    
+    created_product = Product.objects.get(name='Teclado')
+    
+    assert created_product.name == 'Teclado'
+    assert created_product.price == 200
