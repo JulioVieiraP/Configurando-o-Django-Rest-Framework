48841f6ea49b96453b0bd8ee56be059ae880c1f0
JulioVieiraP <134452045+JulioVieiraP@users.noreply.github.com>
2024-08-29 19:11:56 -0300

    feat: criando os testes para user_viewset

--- api/factories.py
+++ api/factories.py
@@ -1,11 +1,15 @@
 import factory
 
-from django.contrib.auth.models import User
-from api.models import Product
+from api.models import Product, User
 
 class UserFactory(factory.django.DjangoModelFactory):
+    name = factory.Faker('name')
     username = factory.Faker('user_name')
+    phone_number = factory.Faker('phone_number')
     email = factory.Faker('email')
+    password = factory.Faker('password')
+    city = factory.Faker('city')
+    address = factory.Faker('address')
     class Meta:
         model = User
     
--- api/tests/tests_serializers/test_user_serializers.py
+++ api/tests/tests_serializers/test_user_serializers.py
@@ -1,26 +1,21 @@
 import pytest
-
-from api.models import User
+from api.models.user import User
 from api.serializers import UserSerializer
 
 @pytest.mark.django_db
 def test_user_creation():
     user = User.objects.create(
-        name = 'testuser',
+        name='testuser',
         username="testeuser123",
-        phone_number = '123456789',
-        email="testeuser123@example.com", 
+        phone_number='123456789',
+        email="testeuser123@example.com",
         password="teste123",
-        city = 'Test City',
-        address = 'Test Address'
+        city='Test City',
+        address='Test Address'
     )
+    
     serializer = UserSerializer(instance=user)
     data = serializer.data
 
-    assert data['name'] == 'testuser'
     assert data['username'] == 'testeuser123'
-    assert data['phone_number'] == '123456789'
     assert data['email'] == 'testeuser123@example.com'
-    assert data['password'] == 'teste123'
-    assert data['city'] == 'Test City'
-    assert data['address'] == 'Test Address'
