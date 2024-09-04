11b908a385131cbbca3911f10fffebe180d5a09d
JulioVieiraP <134452045+JulioVieiraP@users.noreply.github.com>
2024-09-04 18:07:34 -0300

    feat: adicionando o debug_toolbar ao projeto e configurando a url e setting

--- DRF/settings.py
+++ DRF/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django_extensions",
     "rest_framework",
     "api",
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "DRF.urls"
@@ -124,6 +126,10 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
 
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
