d1be82d7458a8a93d32bc780c463047ef71f13b6
JulioVieiraP <134452045+JulioVieiraP@users.noreply.github.com>
2024-09-12 19:22:34 -0300

    feat: adicionando a ordenação para na product viewset

--- api/viewsets/product_viewset.py
+++ api/viewsets/product_viewset.py
@@ -3,6 +3,7 @@ from rest_framework.viewsets import ModelViewSet
 from api.models import Product
 from api.serializers import ProductSerializer
 
+
 class ProductViewSet(ModelViewSet):
-    queryset = Product.objects.all()
+    queryset = Product.objects.all().order_by("id")
     serializer_class = ProductSerializer
